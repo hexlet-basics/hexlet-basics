2f411f1c6d75660b2f9bbe8ef8d72d713abb5434
Artem Rakov <scoff1k@gmail.com>
2020-05-16 00:59:00 -0400

    add html for sessions new

--- app/controllers/web/sessions_controller.rb
+++ app/controllers/web/sessions_controller.rb
@@ -12,7 +12,7 @@ class Web::SessionsController < Web::ApplicationController
       sign_in @user
       redirect_to root_path
     else
-      render :new
+      redirect_to new_sessions_path
     end
   end
 
