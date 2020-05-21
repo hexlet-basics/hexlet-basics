d3e417a1af35a3ba1eb2514f511edc433e775bdb
Artem Rakov <scoff1k@gmail.com>
2020-05-21 14:18:26 -0400

    add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }
+
   def guest?
     false
   end
