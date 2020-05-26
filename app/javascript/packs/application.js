c77a6771c7036c822a3fd85deb7f6e71304eeb68
Vasiliy Kudinev <vk@didlogic.com>
2020-05-26 18:45:42 +0300

    exec webpack react

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import app from './index.jsx';
+import app from './index';
 
 require('jquery');
 require('@rails/ujs').start();
