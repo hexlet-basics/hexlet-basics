4b822697d23492851668e6c6edd9af962ce544d3
Artem Rakov <scoff1k@gmail.com>
2020-05-28 19:01:27 -0400

    updated repository and languages_contoller#index

--- app/repositories/language/module/lesson_repository.rb
+++ app/repositories/language/module/lesson_repository.rb
@@ -4,7 +4,6 @@ module Language::Module::LessonRepository
   extend ActiveSupport::Concern
 
   included do
-    scope :web, -> { for_locale.order(:order) }
-    scope :for_locale, -> { joins(:descriptions).where(language_module_lesson_descriptions: { locale: I18n.locale }) }
+    scope :web, -> { order(:order) }
   end
 end
