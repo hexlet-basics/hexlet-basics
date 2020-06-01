16e190575e588ef7aebcbadc7173289e6fca9401
Artem Rakov <scoff1k@gmail.com>
2020-05-31 23:00:10 -0400

    fix show page

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -2,6 +2,6 @@
 
 class Web::HomeController < Web::ApplicationController
   def index
-    @languages = Language.joins(:current_version).all
+    @languages = Language.includes(:current_version).all
   end
 end
