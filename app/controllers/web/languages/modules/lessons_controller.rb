5e6e01af6c087be182af40dc51c712ed751b933c
Vasiliy Kudinev <vk@didlogic.com>
2020-06-20 18:20:06 +0300

    add gon

--- app/controllers/web/languages/modules/lessons_controller.rb
+++ app/controllers/web/languages/modules/lessons_controller.rb
@@ -5,6 +5,9 @@ class Web::Languages::Modules::LessonsController < Web::Languages::Modules::Appl
     @lesson = resource_module.lessons.find_by!(slug: params[:id])
     @description = @lesson.descriptions.find_by!(locale: I18n.locale)
 
+    current_language_version = @lesson.language.current_version
+    gon.language = current_language_version.name
+
     render :show, layout: 'lesson'
   end
 end
