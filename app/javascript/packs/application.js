0101934dc4de539b858745030520996145947397
Vasiliy Chapaev <morphizm@ya.ru>
2020-07-05 22:04:11 +0300

    add tabler

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'tabler/scss/tabler.scss';
+
 require('jquery');
 require('@rails/ujs').start();
 require('@rails/activestorage').start();
