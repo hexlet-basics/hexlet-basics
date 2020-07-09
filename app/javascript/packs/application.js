534ee7224cdf1a2149a66d4eaa3ef1e6371b95a1
Vasiliy Chapaev <morphizm@ya.ru>
2020-07-09 23:00:33 +0300

    add fontawesome script

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import '@fortawesome/fontawesome-free/js/all.js';
 
 require('jquery');
 require('@rails/ujs').start();
