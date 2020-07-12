ffcda8bf7a261a90379e739d90c6231d554afb15
Artem Rakov <scoff1k@gmail.com>
2020-07-12 02:22:33 -0400

    proper tips and definitions

--- app/mutators/lesson_member_mutator.rb
+++ app/mutators/lesson_member_mutator.rb
@@ -2,11 +2,10 @@
 
 class LessonMemberMutator
   def self.find_or_create_member!(lesson:, lesson_version:, language:, user:)
-    member = lesson.find_by(language: language, user: user)
+    member = lesson.members.find_by(language: language, user: user)
     return member unless member.nil?
 
     member = lesson.members.build(
-      lesson: lesson,
       lesson_version: lesson_version,
       user: user,
       language: language
