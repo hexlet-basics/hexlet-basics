70998dd359ed9ad1b4b97835712a1a8d1cab7986
Artem Rakov <scoff1k@gmail.com>
2020-07-17 21:22:52 -0400

    fix tests

--- app/controllers/web/languages/lessons_controller.rb
+++ app/controllers/web/languages/lessons_controller.rb
@@ -4,7 +4,7 @@ class Web::Languages::LessonsController < Web::Languages::ApplicationController
   def show
     @lesson = resource_language.lessons.find_by!(slug: params[:id])
     @info = @lesson.infos.find_by!(locale: I18n.locale)
-    Language::Lesson::Member.find_or_create_by(lesson: @lesson, language: resource_language, user: current_user) unless current_user.guest?
+    Language::Lesson::Member.find_or_create_by!(lesson: @lesson, language: resource_language, user: current_user) unless current_user.guest?
 
     gon.language = resource_language.to_s
     gon.locale = I18n.locale
