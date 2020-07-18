66603b9094e4d4a29edec14dee16f2fd5539a3e1
Artem Rakov <scoff1k@gmail.com>
2020-07-17 21:13:44 -0400

    imporve checks controller

--- app/controllers/api/lessons/checks_controller.rb
+++ app/controllers/api/lessons/checks_controller.rb
@@ -5,24 +5,22 @@ class Api::Lessons::ChecksController < Api::Lessons::ApplicationController
     lesson_version = resource_lesson.versions.find(params[:data][:attributes][:version_id])
     code = params[:data][:attributes][:code]
 
-    language = resource_lesson.language
-
-    if language.current_lessons.exclude?(lesson_version)
+    if resource_lesson.outdated?(lesson_version)
       return render json: {
-        message: "lesson is outdated or deleted"
-      }, status: 410
+        message: 'lesson is outdated or deleted'
+      }, status: :gone
     end
 
     language_version = lesson_version.language_version
-    data = LessonTester.run(lesson_version, language_version, code, current_user)
+    lesson_exercise_data = LessonTester.run(lesson_version, language_version, code, current_user)
 
-    if data[:passed]
+    if lesson_exercise_data[:passed]
       lesson_member = resource_lesson.members.find_by!(user: current_user)
       lesson_member.finish!
     end
 
     render json: {
-      attributes: data
-    }, status: 200
+      attributes: lesson_exercise_data
+    }, status: :ok
   end
 end
