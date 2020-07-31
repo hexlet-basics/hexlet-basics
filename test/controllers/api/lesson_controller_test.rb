39ebe5a173d90ed47b680b5425ef1379ae340aeb
Vasiliy Chapaev <morphizm@ya.ru>
2020-08-01 00:21:50 +0300

    test for api, lesson

---
+++ test/controllers/api/lesson_controller_test.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class LessonTester
+  def self.run(*)
+    { passed: true }
+  end
+end
+
+class Api::LessonsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @language = languages(:javascript)
+    @lesson = @language.lessons.first
+    @user = users(:one)
+    @lesson_member = @lesson.members.find_or_create_by!(language: @lesson.language, user: @user)
+    sign_in_as(:one)
+  end
+
+  test 'check lesson finished' do
+    post check_api_lesson_path(@lesson), params: { version_id: @lesson.versions.first.id, data: { attributes: { code: 'code' } } }
+
+    assert { response.successful? }
+    @lesson_member.reload
+    assert { @lesson_member.finished? }
+  end
+
+  test 'check language finished' do
+    language_member = @language.members.create!(user: @user)
+    post check_api_lesson_path(@lesson), params: { version_id: @lesson.versions.first.id, data: { attributes: { code: 'code' } } }
+
+    assert { response.successful? }
+    language_member.reload
+    assert { language_member.finished? }
+  end
+end
--- test/controllers/web/languages/lesson_controller_test.rb
+++ test/controllers/web/languages/lesson_controller_test.rb
@@ -4,8 +4,8 @@ require 'test_helper'
 
 class Web::Languages::LessonsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @language = Language.first
-    @lesson = @language.lessons.first
+    @lesson = language_lessons(:two)
+    @language = @lesson.language
   end
 
   test 'show' do
@@ -14,12 +14,20 @@ class Web::Languages::LessonsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'next_lesson' do
+    third_language_lesson = language_lessons(:three)
+
     get next_lesson_language_lesson_path(@language.slug, @lesson.slug)
     assert_response :redirect
+
+    assert_redirected_to language_lesson_path(@language.slug, third_language_lesson.slug)
   end
 
   test 'prev_lesson' do
+    first_language_lesson = language_lessons(:one)
+
     get prev_lesson_language_lesson_path(@language.slug, @lesson.slug)
     assert_response :redirect
+
+    assert_redirected_to language_lesson_path(@language.slug, first_language_lesson.slug)
   end
 end
