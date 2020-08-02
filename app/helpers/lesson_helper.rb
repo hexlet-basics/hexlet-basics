e2356b9e8cff030d088702ef8bd24e4c2112f46e
Vasiliy Chapaev <morphizm@ya.ru>
2020-08-02 12:02:20 +0300

    breadcrumbs

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -20,4 +20,8 @@ module LessonHelper
     markdown = Redcarpet::Markdown.new(renderer, extensions)
     markdown.render(text)
   end
+
+  def current_breadcrumb(lesson_version, lesson_version_info, langugage_lessons_count)
+    "#{lesson_version_info} #{lesson_version.natural_order}/#{langugage_lessons_count}"
+  end
 end
