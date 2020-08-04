27946456e4496d26f5fe1c87d638483297a51d31
Vasiliy Chapaev <morphizm@ya.ru>
2020-08-04 20:05:49 +0300

    hljs to application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import '@fortawesome/fontawesome-free/js/all.js';
+import hljs from 'highlight.js';
 
 require('jquery');
 require('@rails/ujs').start();
@@ -10,6 +11,8 @@ require('@rails/activestorage').start();
 require('channels');
 require('popper.js');
 require('bootstrap');
+
+hljs.initHighlightingOnLoad();
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
