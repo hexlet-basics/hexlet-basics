d54fb1f6cb4e32c6f4f7094be4b707c7a9253c65
Vasiliy Chapaev <morphizm@ya.ru>
2020-08-05 23:01:57 +0300

    add link to current source code lesson

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -24,4 +24,12 @@ module LessonHelper
   def current_breadcrumb(lesson_version, lesson_version_info, langugage_lessons_count)
     "#{lesson_version_info} #{lesson_version.natural_order}/#{langugage_lessons_count}"
   end
+
+  def get_lesson_source_code(lesson_version, lesson_version_info)
+    repository_path = ExternalLinks.source_code
+    locale = lesson_version_info.locale
+    path_to_description = File.join(repository_path, lesson_version.path_to_code, "description.#{locale}.yml")
+
+    path_to_description.gsub('modules', 'blob/master/modules')
+  end
 end
