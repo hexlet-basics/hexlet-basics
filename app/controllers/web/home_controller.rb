f66055db1bdfc007aec6c2b92d0fca46c13fb40f
Artem Rakov <scoff1k@gmail.com>
2020-08-06 22:34:29 -0400

    added any

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -4,7 +4,6 @@ class Web::HomeController < Web::ApplicationController
   def index
     @languges_published = Language.published.includes(:current_version)
     @languages_in_development = Language.in_development.includes(:current_version)
-    @languages_in_development_count = @languages_in_development.count
     @language_members_by_language = current_user.language_members.index_by(&:language_id)
   end
 end
