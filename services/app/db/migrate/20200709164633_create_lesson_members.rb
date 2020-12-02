bf258c544115de981532947f19d7d7b061a5acaa
plugin73 <laplugin73@gmail.com>
2020-12-02 12:21:07 +0000

    fix rename columnd

--- services/app/db/migrate/20200709164633_create_lesson_members.rb
+++ services/app/db/migrate/20200709164633_create_lesson_members.rb
@@ -1,6 +1,7 @@
 class CreateLessonMembers < ActiveRecord::Migration[6.0]
   def change
     rename_table :user_finished_lessons, :language_lesson_members
+    rename_column :language_lesson_members, :language_module_lesson_id, :lesson_id
     add_column :language_lesson_members, :state, :string
     add_column :language_lesson_members, :language_id, :bigint, references: { to_table: :languages }
     Language::Lesson::Member.update_all(state: :finished)
--- services/app/db/migrate/20200709171856_rename_lesson_member_columns.rb
+++ services/app/db/migrate/20200709171856_rename_lesson_member_columns.rb
@@ -1,5 +1,4 @@
 class RenameLessonMemberColumns < ActiveRecord::Migration[6.0]
   def change
-    rename_column :language_lesson_members, :language_module_lesson_id, :lesson_id
   end
 end
