202a5322606d183f9f1287f6676efc5a3cd40df8
plugin73 <laplugin73@gmail.com>
2020-12-02 13:26:29 +0000

    configure session store

---
+++ services/app/config/initializers/session_store.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_hexlet_basics_session',
+                                                      expire_after: 1.month, domain: :all,
+                                                      same_site: :lax
