bf74cfa8367ab0c60c76522610d97e2c2afb9a18
plugin73 <laplugin73@gmail.com>
2020-12-07 08:50:50 +0000

    fix

--- services/app/app/lib/download_exercise.rb
+++ services/app/app/lib/download_exercise.rb
@@ -7,7 +7,7 @@ class DownloadExercise
 
     #FIXME can't pass data in volume inside docker.
     system("docker run --name exercises-#{lang_name} -v /var/tmp/hexletbasics/exercises-#{lang_name}:/out hexletbasics/exercises-#{lang_name}")
-    system("docker cp exercises-#{lang_name}:/exercises-#{lang_name}/* /var/tmp/hexletbasics/")
+    system("docker cp exercises-#{lang_name}:/exercises-#{lang_name} /var/tmp/hexletbasics/")
     system("docker rm exercises-#{lang_name}")
 
     "/var/tmp/hexletbasics/exercises-#{lang_name}"
