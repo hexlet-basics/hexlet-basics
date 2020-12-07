d7cadc60baa15a420f956d31012c5d190eed456c
plugin73 <laplugin73@gmail.com>
2020-12-07 11:57:00 +0000

    find or initalize account

--- services/app/app/services/social_network_service.rb
+++ services/app/app/services/social_network_service.rb
@@ -5,7 +5,7 @@ class SocialNetworkService
     user = User.find_or_initialize_by(email: auth.info.email)
     user.save!
 
-    account = user.accounts.find_or_create_by(provider: auth.provider)
+    account = user.accounts.find_or_initialize_by(provider: auth.provider)
     account.uid = auth.uid
     account.save!
 
