397af1b7ecc43a4150acdeeb8b1403420bdeddf0
plugin73 <laplugin73@gmail.com>
2020-12-07 08:12:02 +0000

    fix docker cp exercise data

--- services/app/app/lib/download_exercise.rb
+++ services/app/app/lib/download_exercise.rb
@@ -4,7 +4,11 @@ class DownloadExercise
   def self.run(lang_name)
     system("docker pull hexletbasics/exercises-#{lang_name}")
     system("rm -rf /var/tmp/hexletbasics/exercises-#{lang_name}")
-    system("docker run --rm -v /var/tmp/hexletbasics/exercises-#{lang_name}:/out hexletbasics/exercises-#{lang_name} bash -c 'cp -r /exercises-#{lang_name}/* /out'")
+
+    #FIXME can't pass data in volume inside docker.
+    system("docker run --name exercises-#{lang_name} -v /var/tmp/hexletbasics/exercises-#{lang_name}:/out hexletbasics/exercises-#{lang_name}"
+    system("docker cp exercises-#{lang_name}:/exercises-#{lang_name}/* /var/tmp/hexletbasics/")
+    system("docker rm exercises-#{lang_name}")
 
     "/var/tmp/hexletbasics/exercises-#{lang_name}"
   end
