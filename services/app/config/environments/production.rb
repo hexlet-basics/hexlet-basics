65f065dc569b44887068e710a9da62ef8d017120
plugin73 <laplugin73@gmail.com>
2020-12-07 09:31:06 +0000

    try handle assets

--- services/app/config/environments/production.rb
+++ services/app/config/environments/production.rb
@@ -22,7 +22,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{2.days.to_i}"
+  }
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
