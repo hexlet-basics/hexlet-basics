f27983441e0af2749d10450385c6fba18bea7e94
Artem Rakov <scoff1k@gmail.com>
2020-12-09 19:53:21 -0500

    fix subject

--- services/app/app/mailers/user_mailer.rb
+++ services/app/app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def reset_password(user_id)
     @user = User.find(user_id)
 
-    mail(to: @user.email, subject: t('.reset_password'))
+    mail(to: @user.email)
   end
 end
