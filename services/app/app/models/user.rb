20591f3445b9b09d570fe1bff1b4fb66f929a1cd
plugin73 <laplugin73@gmail.com>
2020-12-10 12:34:54 +0000

    add states to user

--- services/app/app/models/user.rb
+++ services/app/app/models/user.rb
@@ -2,15 +2,30 @@
 
 class User < ApplicationRecord
   include UserRepository
+  include AASM
 
   has_secure_password validations: false
 
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, 'valid_email_2/email': true
+  # FIXME AASM has not without_state method. We have to change string comparsion to method that will check state
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, 'valid_email_2/email': true, if: -> { state != :removed }
 
   has_many :lesson_members, class_name: 'Language::Lesson::Member', dependent: :destroy
   has_many :lessons, through: :lesson_members, class_name: 'Language::Lesson'
   has_many :language_members, class_name: 'Language::Member', dependent: :destroy
   has_many :accounts, dependent: :destroy
+
+  aasm :state do
+    state :active, initial: true
+    state :removed
+
+    event :activate do
+      transitions from: %i[created, archived], to: :active
+    end
+
+    event :mark_as_archived do
+      transitions from: %i[created, arcive], to: :archived
+    end
+  end
 
   def guest?
     false
