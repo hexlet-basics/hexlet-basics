da0a80054da93580bd349c440acc95b6e968440f
plugin73 <laplugin73@gmail.com>
2020-12-11 08:00:24 +0000

    web scope

--- services/app/app/repositories/language/lesson_repository.rb
+++ services/app/app/repositories/language/lesson_repository.rb
@@ -4,6 +4,7 @@ module Language::LessonRepository
   extend ActiveSupport::Concern
 
   included do
+    scope :web, -> { active }
     scope :left_join_lesson_member_and_user, ->(user) { joins("LEFT OUTER JOIN language_lesson_members ON language_lesson_members.lesson_id = language_lessons.id AND language_lesson_members.user_id = #{user.id}") }
     scope :ordered, -> { includes(:versions).order('language_lesson_versions.natural_order') }
   end
