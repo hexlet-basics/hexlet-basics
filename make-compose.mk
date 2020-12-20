dc35b44502942b2de89438ad5d71ec705164a874
Kirill Mokevnin <mokevnin@gmail.com>
2020-12-20 21:50:33 +0000

    update workflow

--- make-compose.mk
+++ make-compose.mk
@@ -12,6 +12,9 @@ compose-restart:
 
 compose-setup: compose-down compose-build app-setup
 
+compose-ci-build:
+	docker-compose -f docker-compose.yml build
+
 compose-ci:
 	docker-compose -f docker-compose.yml up
 
