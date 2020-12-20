92aa49efea10b5185fd24d68ce91cb3419c6a531
Kirill Mokevnin <mokevnin@gmail.com>
2020-12-20 22:31:36 +0000

    update workflow

--- services/app/config/webpack/production.js
+++ services/app/config/webpack/production.js
@@ -2,4 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
 
+environment.plugins.delete('Compression Brotli');
+environment.plugins.delete('Compression');
+
 module.exports = environment.toWebpackConfig();
