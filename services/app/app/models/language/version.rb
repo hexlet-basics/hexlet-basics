2d05bae53a93d8eaacf6d522af8babe3846ac221
grozwalker <grozwalker@gmail.com>
2020-12-21 11:49:30 +0000

    add inverse of

--- services/app/app/models/language/version.rb
+++ services/app/app/models/language/version.rb
@@ -3,10 +3,22 @@
 class Language::Version < ApplicationRecord
   include AASM
 
-  has_many :module_versions, dependent: :destroy, foreign_key: :language_version_id, class_name: 'Language::Module::Version'
-  has_many :lesson_versions, dependent: :destroy, foreign_key: :language_version_id, class_name: 'Language::Lesson::Version'
-  has_many :lesson_infos, dependent: :destroy, foreign_key: :language_version_id, class_name: 'Language::Lesson::Version::Info'
-  has_many :module_infos, dependent: :destroy, foreign_key: :language_version_id, class_name: 'Language::Module::Version::Info'
+  has_many :module_versions, dependent: :destroy,
+                             foreign_key: :language_version_id,
+                             class_name: 'Language::Module::Version',
+                             inverse_of: :language_version
+  has_many :lesson_versions, dependent: :destroy,
+                             foreign_key: :language_version_id,
+                             class_name: 'Language::Lesson::Version',
+                             inverse_of: :language_version
+  has_many :lesson_infos, dependent: :destroy,
+                          foreign_key: :language_version_id,
+                          class_name: 'Language::Lesson::Version::Info',
+                          inverse_of: :language_version
+  has_many :module_infos, dependent: :destroy,
+                          foreign_key: :language_version_id,
+                          class_name: 'Language::Module::Version::Info',
+                          inverse_of: :language_version
   has_many :lessons, through: :lesson_versions
 
   belongs_to :language
