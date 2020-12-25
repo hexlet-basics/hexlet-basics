3964321468ff91294831b1b78fc4e09a1509b64d
Daniyar Zhanakhmetov <49826703+f1eeman@users.noreply.github.com>
2020-12-25 19:24:28 +0600

    update placement of border in Editor (#99)

--- services/app/app/javascript/packs/lesson/components/HTMLPreview.jsx
+++ services/app/app/javascript/packs/lesson/components/HTMLPreview.jsx
@@ -5,7 +5,7 @@ import Frame from 'react-frame-component';
 const HTMLPreview = (props) => {
   const { html } = props;
   return (
-    <div className="mt-2 p-2 h-50 bg-white">
+    <div className="pt-3 px-2 pb-2 h-50 bg-white border-top">
       <Frame className="border-0 h-100 w-100">
         <div className="text-black" dangerouslySetInnerHTML={{ __html: html }} />
       </Frame>
