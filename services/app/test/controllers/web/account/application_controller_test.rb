bd28642350236f6d029362a687abc244d2e5be74
Andrey Groza <grozwalker@gmail.com>
2021-01-13 21:04:11 +0000

    fix lint

--- services/app/test/controllers/web/account/application_controller_test.rb
+++ services/app/test/controllers/web/account/application_controller_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class Web::Account::ApplicationControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
