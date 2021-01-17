a52a545ebb6a803d81632c13f8c55decdc4dddbd
Artem Rakov <scoff1k@gmail.com>
2021-01-17 20:18:56 +0000

    fix root admin view

--- app/controllers/web/admin/home_controller.rb
+++ app/controllers/web/admin/home_controller.rb
@@ -3,6 +3,7 @@
 class Web::Admin::HomeController < Web::Admin::ApplicationController
   def index
     @q = User.admin.ransack(params[:q])
+    @q.sorts = 'created_at desc' if @q.sorts.empty?
     @users = @q.result
   end
 end
