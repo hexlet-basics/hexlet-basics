07114a008a13a3c664bce005e8d34cde0d4534ef
Kirill Mokevnin <mokevnin@gmail.com>
2021-01-17 04:06:27 +0000

    fix webpacker

---
+++ app/helpers/packs_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# rubocop:disable Rails/HelperInstanceVariable
+module PacksHelper
+  def append_javascript_packs(*packs)
+    @javascript_packs ||= []
+    @javascript_packs.push(*packs)
+  end
+
+  def prepend_javascript_packs(*packs)
+    @javascript_packs ||= []
+    @javascript_packs.unshift(*packs)
+  end
+
+  def render_javascript_stored_packs
+    javascript_packs_with_chunks_tag(*@javascript_packs, crossorigin: 'anonymous')
+  end
+end
+# rubocop:enable Rails/HelperInstanceVariable
