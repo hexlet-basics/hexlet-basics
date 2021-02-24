17d9582b9e1d416b097810e97677a0443cc8c746
Kirill Mokevnin <mokevnin@gmail.com>
2021-02-24 17:12:31 +0000

    fix webpacker
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,7 +1,22 @@
 const { webpackConfig, merge } = require('@rails/webpacker');
 const CompressionPlugin = require('compression-webpack-plugin');
+const WebpackAssetsManifest = require('webpack-assets-manifest');
 
 const customConfig = require('./environment.js');
+
+// https://github.com/rails/webpacker/issues/2864
+webpackConfig.plugins.splice(2, 1);
+webpackConfig.plugins.push(
+  new WebpackAssetsManifest({
+    enabled: true,
+    entrypoints: true,
+    writeToDisk: true,
+    output: 'manifest.json',
+    entrypointsUseAssets: true,
+    space: 2,
+    publicPath: true,
+  }),
+);
 
 // https://github.com/rails/webpacker/issues/2399
 webpackConfig.plugins = webpackConfig.plugins.filter((p) => !(p instanceof CompressionPlugin));
