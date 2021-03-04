423b3e62279a4e900bf360e260aa361a4fa1e5e4
Vagin <laplugin73@gmail.com>
2021-03-04 08:43:34 +0000

    new kub cluster

--- terraform/dok8s.tf
+++ terraform/dok8s.tf
@@ -10,3 +10,18 @@ resource "digitalocean_kubernetes_cluster" "hexlet_basics_2" {
     node_count = 2
   }
 }
+
+resource "digitalocean_kubernetes_cluster" "hexlet-basics-4" {
+  version = "1.20.2-do.0"
+
+  name = "hexlet-basics-4"
+  region = "fra1"
+
+  auto_upgrade = true
+
+  node_pool {
+    name       = "hexlet-basics-node-pool-4"
+    size       = "c-2"
+    node_count = 3
+  }
+}
