3183e82aa3116138f65f2ffef5796621ea6a2bf0
Roman Makarov <deepspiral@yandex.ru>
2021-05-27 14:12:44 +0000

    remove prop-types

--- app/packs/src/lesson/components/HTMLPreview.jsx
+++ app/packs/src/lesson/components/HTMLPreview.jsx
@@ -1,22 +1,14 @@
 // @ts-check
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import Frame from 'react-frame-component';
 
-const HTMLPreview = (props) => {
-  const { html } = props;
-  return (
-    <div className="pt-3 px-2 pb-2 h-50 bg-white border-top">
-      <Frame className="border-0 h-100 w-100">
-        <div className="text-black" dangerouslySetInnerHTML={{ __html: html }} />
-      </Frame>
-    </div>
-  );
-};
-
-HTMLPreview.propTypes = {
-  html: PropTypes.string.isRequired,
-};
+const HTMLPreview = ({ html }) => (
+  <div className="pt-3 px-2 pb-2 h-50 bg-white border-top">
+    <Frame className="border-0 h-100 w-100">
+      <div className="text-black" dangerouslySetInnerHTML={{ __html: html }} />
+    </Frame>
+  </div>
+);
 
 export default HTMLPreview;
