20d554e5599e7e42426741de927777c306552662
Kirill Mokevnin <mokevnin@gmail.com>
2021-05-28 11:13:43 +0000

    improve language landing
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -7,6 +7,22 @@
 
 import '../stylesheets/application.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'programming-languages-logos/src/javascript/javascript.svg';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'programming-languages-logos/src/java/java.svg';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'programming-languages-logos/src/php/php.svg';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'programming-languages-logos/src/python/python.svg';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'programming-languages-logos/src/ruby/ruby.svg';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'programming-languages-logos/src/html/html.svg';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'programming-languages-logos/src/css/css.svg';
+
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
@@ -24,5 +40,6 @@ hljs.highlightAll();
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// const images = require.context('../images', true);
+// const imagePath = (name) => images(name, true);
+// console.log(imagePath);
