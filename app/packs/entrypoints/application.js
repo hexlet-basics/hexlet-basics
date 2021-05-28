c1ad2e8c24fc8c08559ce0a3d318da722640f6c0
Vagin <laplugin73@gmail.com>
2021-05-28 12:40:16 +0000

    fix

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -37,6 +37,7 @@ require('bootstrap');
 hljs.highlightAll();
 
 const test = "";
+console.log(test);
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
