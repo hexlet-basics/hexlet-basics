4d35033f99fbb7780996630f6acf791e853e646e
Andrey Groza <grozwalker@gmail.com>
2021-05-31 14:14:22 +0000

    add lesson version to_s method

--- app/models/language/lesson/version.rb
+++ app/models/language/lesson/version.rb
@@ -8,6 +8,10 @@ class Language::Lesson::Version < ApplicationRecord
 
   has_many :infos, dependent: :destroy
 
+  def to_s
+    slug
+  end
+
   def next_lesson
     language_version
       .lesson_versions.order(:natural_order)
