8ad8b01b4baea94de8ed27cb01b7b3ebac0ea9c1
Roman Makarov <deepspiral@yandex.ru>
2021-06-01 13:14:17 +0000

    fix linter offence

--- app/packs/entrypoints/pageWelcome.js
+++ app/packs/entrypoints/pageWelcome.js
@@ -1,5 +1,6 @@
 import Typed from 'typed.js';
 
+// eslint-disable-next-line no-unused-vars
 const typed = new Typed('.hero-header', {
   strings: ['JavaScript', 'PHP', 'Java', 'HTML', 'CSS', 'Ruby', 'Python'],
   typeSpeed: 50,
