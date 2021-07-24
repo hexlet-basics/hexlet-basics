0d1690ec37f3c7b0d870d894338c80435d7ad96f
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-24 15:28:52 +0000

    fix #155
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Web::ApplicationController < ApplicationController
+  include ActionView::Helpers::UrlHelper
   include FlashConcern
   include TitleConcern
   include EventConcern
@@ -8,21 +9,30 @@ class Web::ApplicationController < ApplicationController
   before_action do
     return if browser.bot?
 
-    # TODO: завести определение локали на базе ip
-    # results = Geocoder.search(request.remote_ip)
+    I18n.locale = request.subdomains.first || :en
 
-    locale = (current_user.locale || session[:locale] ||
-              request.subdomains.first || :ru).to_sym
+    subdomains = {
+      ru: 'ru'
+    }
 
-    if locale == :ru && request.subdomains.empty?
-      redirect_to url_for(params.merge(subdomain: locale, only_path: false).permit!)
+    results = Geocoder.search(request.remote_ip)
+    result = results.first
+    subdomain = request.subdomains.first
+    ru_country_codes = ['RU']
+    remembered_locale = session[:locale].to_sym
+
+    if current_page?(root_path)
+      if remembered_locale && remembered_locale != I18n.locale
+        url = root_url(subdomain: subdomains.fetch(remembered_locale, ''))
+        redirect_to url
+      elsif !remembered_locale && !subdomain && ru_country_codes.include?(result.country)
+        url = root_url(subdomain: 'ru')
+        redirect_to url
+      elsif !subdomain && ru_country_codes.exclude?(result.country)
+        # Говорим о том, что в английском пока не очень много контента и возможно вы хотели русский
+        # f(:, now: true)
+      end
     end
-
-    if locale == :en && request.subdomains.any?
-      redirect_to url_for(params.merge(subdomain: nil, only_path: false).permit!)
-    end
-
-    I18n.locale = locale
   end
 
   before_action do
