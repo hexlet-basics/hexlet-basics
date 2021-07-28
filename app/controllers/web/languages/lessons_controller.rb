b4e8c49356b964139e5309b64b16a25a5ec12802
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-28 16:51:10 +0000

    fix litner errors
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/controllers/web/languages/lessons_controller.rb
+++ app/controllers/web/languages/lessons_controller.rb
@@ -32,7 +32,6 @@ class Web::Languages::LessonsController < Web::Languages::ApplicationController
     set_meta_tags canonical: language_lesson_url(@lesson.language.slug, @lesson.slug),
                   amphtml: language_lesson_url(@lesson.language.slug, @lesson.slug, format: 'amp', only_path: false),
                   title: [@info, t("human_languages.#{@resource_language}")].join(' | ')
-
   end
 
   def next_lesson
