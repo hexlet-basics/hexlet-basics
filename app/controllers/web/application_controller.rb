e0a8b272c33b0e6c12b4a62bd1ac1f9d2a368d85
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-28 06:21:01 +0000

    fix locale for bots
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -23,30 +23,31 @@ class Web::ApplicationController < ApplicationController
   private
 
   def prepare_locale_settings
-    return if browser.bot?
-
     I18n.locale = request.subdomains.first || :en
 
-    subdomains = {
-      ru: 'ru'
-    }
+    # NOTE: боты не должны попадать на автоматический редирект
+    unless browser.bot?
+      subdomains = {
+        ru: 'ru'
+      }
 
-    results = Geocoder.search(request.remote_ip)
-    result = results.first
-    subdomain = request.subdomains.first
-    ru_country_codes = ['RU']
-    remembered_locale = session[:locale]&.to_sym
+      results = Geocoder.search(request.remote_ip)
+      result = results.first
+      subdomain = request.subdomains.first
+      ru_country_codes = ['RU']
+      remembered_locale = session[:locale].to_sym
 
-    if current_page?(root_path)
-      if remembered_locale && remembered_locale != I18n.locale
-        url = root_url(subdomain: subdomains.fetch(remembered_locale, ''))
-        redirect_to url
-      elsif !remembered_locale && !subdomain && ru_country_codes.include?(result.country)
-        url = root_url(subdomain: 'ru')
-        redirect_to url
-      elsif !subdomain && ru_country_codes.exclude?(result.country)
-        # Говорим о том, что в английском пока не очень много контента и возможно вы хотели русский
-        # f(:, now: true)
+      if current_page?(root_path)
+        if remembered_locale && remembered_locale != I18n.locale
+          url = root_url(subdomain: subdomains.fetch(remembered_locale, ''))
+          redirect_to url
+        elsif !remembered_locale && !subdomain && ru_country_codes.include?(result.country)
+          url = root_url(subdomain: 'ru')
+          redirect_to url
+        elsif !subdomain && ru_country_codes.exclude?(result.country)
+          # Говорим о том, что в английском пока не очень много контента и возможно вы хотели русский
+          # f(:, now: true)
+        end
       end
     end
   end
