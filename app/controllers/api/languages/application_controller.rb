39d5468dd33371694b98fff2e6a9f7ac79d612af
Andrey Groza <grozwalker@gmail.com>
2021-10-20 15:34:31 +0000

    memorize resource vars

--- app/controllers/api/languages/application_controller.rb
+++ app/controllers/api/languages/application_controller.rb
@@ -4,10 +4,10 @@ class Api::Languages::ApplicationController < Api::ApplicationController
   helper_method :resource_language
 
   def resource_language
-    @resource_langauge = Language.find(params[:language_id])
+    @resource_language ||= Language.find(params[:language_id])
   end
 
   def resource_language_version
-    @resource_langauge_version = resource_language.current_version
+    @resource_language_version ||= resource_language.current_version
   end
 end
