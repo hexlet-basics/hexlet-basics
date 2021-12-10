c1427e751e8eb98c365fe2e2b3ddfc6fa9749677
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-12-10 22:22:43 +0200

    increase test running timeout to 5s

--- app/lib/docker_exercise_api.rb
+++ app/lib/docker_exercise_api.rb
@@ -17,7 +17,7 @@ class DockerExerciseApi
 
   def self.run_exercise(created_code_file_path:, exercise_file_path:, docker_image:, image_tag:, path_to_code:)
     volume = "-v #{created_code_file_path}:#{exercise_file_path}"
-    command = "docker run --rm --net none #{volume} #{docker_image}:#{image_tag} timeout 4 make --silent -C #{path_to_code} test"
+    command = "docker run --rm --net none #{volume} #{docker_image}:#{image_tag} timeout 5 make --silent -C #{path_to_code} test"
 
     output = []
     status = BashRunner.start(command) { |line| output << line }
