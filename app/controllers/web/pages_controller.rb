aa052908bd3ebfe1db0a48a846dd1b344b0eb797
Andrey Moshkov <fakesupp@gmail.com>
2022-03-30 14:24:44 +0000

    fix missing pages translate

--- app/controllers/web/pages_controller.rb
+++ app/controllers/web/pages_controller.rb
@@ -2,7 +2,7 @@
 
 class Web::PagesController < Web::ApplicationController
   def show
-    set_meta_tags title: t(params[:id], scope: 'pages')
+    set_meta_tags title: t(params[:id], scope: 'web.pages')
 
     render params[:id]
   end
