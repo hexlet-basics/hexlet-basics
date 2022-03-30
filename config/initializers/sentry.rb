7079442680d86b02177cb50908cb507909bb87bd
Kirill Mokevnin <mokevnin@gmail.com>
2022-03-29 21:33:33 -0400

    fix linter errors
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,6 @@
 
 Sentry.init do |config|
   config.dsn = 'https://9aa813134b4a4cd2a5700ace36241a58@o1090356.ingest.sentry.io/6298540'
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.traces_sample_rate = 0.5
 end
