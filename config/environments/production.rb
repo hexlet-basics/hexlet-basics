4e479ee3d80bd29921f14cd4afc1969eb9de8c8e
Andrey Grozs <grozwalker@gmail.com>
2022-03-31 08:50:56 +0000

    enable sidekiq for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
+  config.active_job.queue_adapter = :sidekiq
   # config.active_job.queue_name_prefix = "hexlet_basics_production"
 
   config.action_mailer.perform_caching = false
