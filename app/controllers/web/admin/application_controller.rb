16f6239adc83e1153f60ceba21be6d4d87b35b10
Andrey Moshkov <fakesupp@gmail.com>
2022-03-31 08:02:11 +0000

    use shared head to admin layout

--- app/controllers/web/admin/application_controller.rb
+++ app/controllers/web/admin/application_controller.rb
@@ -2,4 +2,8 @@
 
 class Web::Admin::ApplicationController < Web::ApplicationController
   before_action :authenticate_admin!
+
+  before_action do
+    set_meta_tags title: t('title', scope: 'web.admin')
+  end
 end
