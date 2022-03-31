9f3a4903aff56fb14f5c191f6e41b80b5497ff5d
Andrey Moshkov <fakesupp@gmail.com>
2022-03-31 11:28:36 +0000

    use localed languages to header widjet

--- app/javascript/welcome-page.js
+++ app/javascript/welcome-page.js
@@ -1,10 +1,12 @@
 // @ts-check
 
 import Typed from 'typed.js';
+// eslint-disable-next-line import/no-unresolved
+import gon from 'gon';
 
 // eslint-disable-next-line no-unused-vars
 const typed = new Typed('.hero-header', {
-  strings: ['JavaScript', 'PHP', 'Java', 'HTML', 'CSS', 'Ruby', 'Python'],
+  strings: gon.languages_for_widget,
   typeSpeed: 50,
   backSpeed: 50,
   loop: true,
