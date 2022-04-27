917f7a70c50050b5142cf005ab8ec842ddfea28b
Andrey Grozs <grozwalker@gmail.com>
2022-04-27 12:24:39 +0000

    fix locale for api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,16 @@ class ApplicationController < ActionController::Base
 
   include AuthConcern
   content_security_policy Rails.env.production?
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = extract_locale_from_subdomain || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 end
