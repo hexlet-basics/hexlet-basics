990bffe22e8387a71d85d131b26ad0e2555a90b8
Andrey Grozs <grozwalker@gmail.com>
2022-04-27 14:51:14 +0000

    change language queries

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -24,7 +24,7 @@ class Web::ApplicationController < ApplicationController
 
   before_action do
     @language_version_infos = Language::Version::Info
-                              .where({ locale: I18n.locale })
+                              .with_locale
                               .joins(language_version: :current_language)
   end
 
--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -4,12 +4,13 @@ class Web::HomeController < Web::ApplicationController
   def index
     @languages_completed = Language.with_progress(:completed)
                                    .joins(current_version: :infos)
-                                   .where(current_version: { language_version_infos: { locale: I18n.locale } })
+                                   .merge(Language::Version::Info.with_locale)
+
     @languages_in_development = Language.with_progress(:in_development).includes(:current_version)
     @language_members_by_language = current_user.language_members.index_by(&:language_id)
-    # @language_member_fake = Language::MemberFake.new
 
-    languages_for_widget = @languages_completed.pluck('current_version.name')
-    gon.languages_for_widget = languages_for_widget
+    language_versions = Language::Version.where(current_language: @languages_completed)
+
+    gon.languages_for_widget = language_versions.pluck(:name)
   end
 end
