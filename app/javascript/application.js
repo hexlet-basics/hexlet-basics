8c3105b70bc05401f47e0963256da767efe4b2a3
Andrey Grozs <grozwalker@gmail.com>
2022-05-19 11:59:45 +0000

    decrease sentry trace sample rate

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -16,7 +16,7 @@ Sentry.init({
   dsn: 'https://9aa813134b4a4cd2a5700ace36241a58@o1090356.ingest.sentry.io/6298540',
   // release: "my-project-name@2.3.12",
   integrations: [new BrowserTracing()],
-  tracesSampleRate: 0.5,
+  tracesSampleRate: 0.02,
 });
 
 railsUjs.start();
