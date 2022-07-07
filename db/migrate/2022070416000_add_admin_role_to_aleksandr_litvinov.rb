7277dd2fa58d39e500e4a10847e7462ef541eff1
Andrey Groza <grozwalker@gmail.com>
2022-07-07 15:46:10 +0000

    fix migration

--- db/migrate/2022070416000_add_admin_role_to_aleksandr_litvinov.rb
+++ db/migrate/2022070416000_add_admin_role_to_aleksandr_litvinov.rb
@@ -1,4 +1,4 @@
-class AddAdminsRoleToAleksandrLitvinov < ActiveRecord::Migration[7.0]
+class AddAdminRoleToAleksandrLitvinov < ActiveRecord::Migration[7.0]
   def change
     User.find_by(email: 'ssssank@gmail.com')&.update(admin: true)
   end
