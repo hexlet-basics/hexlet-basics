76cde52b2edaa0f1f4caf4054dea940c13f30829
Andrey Groza <grozwalker@gmail.com>
2022-09-02 10:35:10 +0000

    remove unneccery migration

--- db/migrate/20220831102248_set_lang_member_field_not_null.rb
+++
@@ -1,9 +0,0 @@
-class SetLangMemberFieldNotNull < ActiveRecord::Migration[7.0]
-  def up
-    change_column :language_lesson_members, :language_member_id, :integer, null: false
-  end
-
-  def down
-    change_column :language_lesson_members, :language_member_id, :integer, null: true
-  end
-end
