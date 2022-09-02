2a4e56b9ba9eaac42da8850d78e5c37ae235a0e5
Andrey Groza <grozwalker@gmail.com>
2022-09-02 10:13:34 +0000

    revert not null lesson member id field

--- db/migrate/20220831102248_set_lang_member_field_not_null.rb
+++ db/migrate/20220831102248_set_lang_member_field_not_null.rb
@@ -1,5 +1,9 @@
 class SetLangMemberFieldNotNull < ActiveRecord::Migration[7.0]
-  def change
+  def up
     change_column :language_lesson_members, :language_member_id, :integer, null: false
+  end
+
+  def down
+    change_column :language_lesson_members, :language_member_id, :integer, null: true
   end
 end
