10a5454267ca175ccbfb02834af1f522acaa6889
Andrey Groza <grozwalker@gmail.com>
2022-09-05 14:07:21 +0000

    remove migration

--- db/migrate/20220905131707_set_lang_member_field_not_null.rb
+++
@@ -1,5 +0,0 @@
-class SetLangMemberFieldNotNull < ActiveRecord::Migration[7.0]
-  def change
-    change_column :language_lesson_members, :language_member_id, :integer, null: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_132713) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_152823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
