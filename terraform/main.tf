dc1b0038a7b77f0ccc766fa756d97b630af888c9
Andrey Groza <grozwalker@gmail.com>
2022-09-14 14:21:38 +0000

    upgrade k8s version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,7 @@
-data "digitalocean_kubernetes_versions" "last_minor_version" {
-  version_prefix = "1.21."
-}
-
 # --------------------------------------
 # PRIMARY CLUSTER
 # --------------------------------------
 
-# TODO Добавить мониторы https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/monitor_alert
 resource "digitalocean_kubernetes_cluster" "hexlet_basics_cluster_2" {
   name         = var.cluster_name_2
   region       = var.cluster_region
