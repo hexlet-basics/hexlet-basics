ae77ac5a528e572e42f46757bbdacf37f696f9d8
Andrey Moshkov <fakesupp@gmail.com>
2022-11-11 08:42:16 +0000

    fix slack link

--- app/assets/locales/en.js
+++ app/assets/locales/en.js
@@ -9,7 +9,7 @@ export default {
     },
     errors: {
       network: 'There was a network problem. Please try again. If it doesn’t work, make sure you have good internet and no blockers.',
-      server: 'Error on server. Maybe it’ll let go soon, but maybe not. Try to find out what happened in https://slack-ru.hexlet.io/',
+      server: 'Error on server. Maybe it’ll let go soon, but maybe not. Try to find out what happened in https://slack.hexlet.io/',
     },
     run: 'Run',
     resetCode: 'Reset code',
