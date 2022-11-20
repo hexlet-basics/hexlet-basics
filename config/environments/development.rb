86beefd59d31671866a8364864b48621bc6a7191
Kirill Mokevnin <mokevnin@gmail.com>
2022-11-20 12:16:33 -0500

    add terms agreement to sign up form
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
     Bullet.bullet_logger = true
     Bullet.console       = true
     Bullet.rails_logger  = true
-    Bullet.add_footer    = true
+    Bullet.add_footer    = false
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
