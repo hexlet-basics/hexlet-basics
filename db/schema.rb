0b2b45b2cd65bbccfdaca6dfba3bfa2b85b5e3f6
ralder <sergey_mishin@icloud.com>
2022-11-25 09:50:01 +0000

    :card_file_box: populate categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_210951) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_103200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
