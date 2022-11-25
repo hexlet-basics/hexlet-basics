25da3132e17e9b44d5b05204a5413f999beabacc
ralder <sergey_mishin@icloud.com>
2022-11-25 18:54:45 +0000

    add forgotten hljs langs

--- app/javascript/lib/hljs.js
+++ app/javascript/lib/hljs.js
@@ -9,8 +9,33 @@ import sql from 'highlight.js/lib/languages/sql';
 import scss from 'highlight.js/lib/languages/scss';
 import css from 'highlight.js/lib/languages/css';
 import xml from 'highlight.js/lib/languages/xml';
+import scheme from 'highlight.js/lib/languages/scheme';
+import clojure from 'highlight.js/lib/languages/clojure';
+import c from 'highlight.js/lib/languages/c';
+import csharp from 'highlight.js/lib/languages/csharp';
+import typescript from 'highlight.js/lib/languages/typescript';
+import cpp from 'highlight.js/lib/languages/cpp';
+import go from 'highlight.js/lib/languages/go';
+import lua from 'highlight.js/lib/languages/lua';
+import prolog from 'highlight.js/lib/languages/prolog';
+import swift from 'highlight.js/lib/languages/swift';
+import kotlin from 'highlight.js/lib/languages/kotlin';
+import elixir from 'highlight.js/lib/languages/elixir';
+import bash from 'highlight.js/lib/languages/bash';
+import fortran from 'highlight.js/lib/languages/fortran';
+import haskell from 'highlight.js/lib/languages/haskell';
+import rust from 'highlight.js/lib/languages/rust';
 
 const langs = {
+  rust,
+  haskell,
+  fortran,
+  bash,
+  elixir,
+  kotlin,
+  swift,
+  prolog,
+  lua,
   javascript,
   ruby,
   shell,
@@ -22,6 +47,13 @@ const langs = {
   css,
   html: xml,
   xml,
+  clojure,
+  scheme,
+  c,
+  csharp,
+  typescript,
+  cpp,
+  go,
 };
 
 Object.keys(langs).forEach((key) => hljs.registerLanguage(key, langs[key]));
