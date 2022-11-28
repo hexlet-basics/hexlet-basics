f3e0185a3a3526612c0dafccf10abdb9705d6086
Kirill Mokevnin <mokevnin@gmail.com>
2022-11-28 23:41:06 +0000

    add aasm
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+gem 'aasm'
 gem 'aws-sdk-s3'
 
 gem 'configus'
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,16 +2,31 @@
 
 class BlogPost < ApplicationRecord
   extend Enumerize
+  include AASM
 
   enumerize :locale, in: I18n.available_locales
 
   validates :name, presence: true
-  validates :slug, presence: true, uniqueness: true
+  validates :slug, presence: true, uniqueness: true, format: { with: /\A\w{30}\z/ }
   validates :locale, presence: true
   validates :body, presence: true
 
   belongs_to :language, optional: true
   belongs_to :creator, class_name: 'User'
+
+  # aasm do
+  #   state :draft, initial: true
+  #   state :published
+  #   state :archived
+  #
+  #   event :publish do
+  #     transitions to: :
+  #   end
+  #
+  #   event :archive do
+  #     transitions from: :running, to: :cleaning
+  #   end
+  # end
 
   def to_s
     name
