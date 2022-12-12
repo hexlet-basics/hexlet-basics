849a3e021e252b6c61c4258e7c3d22e23d93f010
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2022-12-12 17:15:13 +0000

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@
 
 # require 'active_support/core_ext/integer/time'
 
-Rails.applicationconfigure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
