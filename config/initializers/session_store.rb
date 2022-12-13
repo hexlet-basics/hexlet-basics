157e1dfc6efa6b9b9eac92d26d9f55f7fb0be416
ralder <sergey_mishin@icloud.com>
2022-12-13 08:30:39 +0000

    fix tests

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,6 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_hexlet_basics_session',
-                                                      expire_after: 1.month, domain: :all,
+                                                      expire_after: 1.month,
+                                                      domain: Rails.env.test? ? ENV.fetch('APP_HOST') : :all, # NOTE: :all not working on test env
                                                       same_site: :lax
