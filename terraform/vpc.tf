db353cd86905f8202098ae69db81d503a07ca018
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2024-09-26 09:09:08 +0000

    add firewall for databases

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,3 +3,45 @@ resource "twc_vpc" "hexlet_basics" {
   subnet_v4 = "10.3.1.0/24"
   location = var.location
 }
+
+resource "twc_firewall" "databases" {
+  name = "Hexlet basic DB firewall"
+
+  # NOTE: timeweb terraform provider doesn't support linking database resources to firewall
+  # therefore databases were added manually from control panel
+  # link {
+  #   id = resource.twc_database_cluster.postgresql.id
+  #   type = "server"
+  # }
+
+  # link {
+  #   id = resource.twc_database_cluster.redis.id
+  #   type = "server"
+  # }
+}
+
+resource "twc_firewall_rule" "own_subnet" {
+  firewall_id = resource.twc_firewall.databases.id
+
+  direction = "ingress"
+  protocol = "tcp"
+  cidr = twc_vpc.hexlet_basics.subnet_v4
+}
+
+resource "twc_firewall_rule" "timeweb_monitoring" {
+  firewall_id = resource.twc_firewall.databases.id
+
+  direction = "ingress"
+  port = 10050
+  protocol = "tcp"
+  cidr = "92.53.116.0/24"
+}
+
+
+resource "twc_firewall_rule" "out_tcp" {
+  firewall_id = resource.twc_firewall.databases.id
+
+  direction = "egress"
+  protocol = "tcp"
+  cidr = "0.0.0.0/0"
+}
