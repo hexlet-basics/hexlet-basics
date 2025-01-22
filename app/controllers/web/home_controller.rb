b221a04d568352d257c9e6e79bf465a83790583a
Liza Chernyshova <liz4chernyshova@yandex.ru>
2025-01-22 10:18:51 +0300

    fix condition

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Web::HomeController < Web::ApplicationController
-  before_action -> { @top_banner_name = :competition_banner }, only: %i[index]
-
   def index
     @languages_in_development = Language.with_progress(:in_development).includes(:current_version)
     @language_members_by_language = current_user.language_members.index_by(&:language_id)
