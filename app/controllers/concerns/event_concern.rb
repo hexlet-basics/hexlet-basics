a4ccfb439b53e76aa5bbcd9e0e251ceace302ff7
Kirill Mokevnin <mokevnin@gmail.com>
2025-01-26 17:41:15 -0500

    fix tests
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/controllers/concerns/event_concern.rb
+++ app/controllers/concerns/event_concern.rb
@@ -1,28 +1,28 @@
 # frozen_string_literal: true
 
-# module EventConcern
-#   extend ActiveSupport::Concern
-#
-#   included do
-#     before_action :fire_js_events
-#     after_action :clean_js_events
-#   end
-#
-#   def js_event(event_name, options = {})
-#     EventsMapping.exists!(event_name)
-#     session[:fired_events][event_name] = options
-#   end
-#
-#   def clean_js_events
-#     # NOTE: могут быть двойные редиректы и стираем данные только
-#     # при отрисовке хтмл страницы, а не запросу по апи
-#     if response.successful? && response.content_type.include?("text/html")
-#       session[:fired_events] = {}
-#     end
-#   end
-#
-#   def fire_js_events
-#     session[:fired_events] ||= {}
-#     gon.push({ fired_events: session[:fired_events] })
-#   end
-# end
+module EventConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :fire_js_events
+    after_action :clean_js_events
+  end
+
+  def js_event(event_name, options = {})
+    EventsMapping.exists!(event_name)
+    session[:fired_events][event_name] = options
+  end
+
+  def clean_js_events
+    # NOTE: могут быть двойные редиректы и стираем данные только
+    # при отрисовке хтмл страницы, а не запросу по апи
+    if response.successful? && response.content_type.include?("text/html")
+      session[:fired_events] = {}
+    end
+  end
+
+  def fire_js_events
+    session[:fired_events] ||= {}
+    gon.push({ fired_events: session[:fired_events] })
+  end
+end
