3a5abf0f1ae542260112aa037ef77128268674ac
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2025-01-29 14:52:20 +0000

    enable sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,19 +1,19 @@
-# Sentry.init do |config|
-#   config.release = "hexle-basics-platform@#{configus.hexlet_basics_release_version}"
-#
-#   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-#   config.traces_sample_rate = 0.002
-#
-#   config.send_default_pii = true
-#   config.send_modules = false
-#   config.rails.report_rescued_exceptions = true
-#
-#   config.excluded_exceptions += [
-#     'ActionController::RoutingError',
-#     'ActionController::UnknownFormat',
-#     'ActiveRecord::RecordNotFound',
-#     'Faraday::ConnectionFailed',
-#     'Pundit::NotAuthorizedError',
-#     'Mime::Type::InvalidMimeType'
-#   ]
-# end
+Sentry.init do |config|
+  config.release = "hexle-basics-platform@#{configus.hexlet_basics_release_version}"
+
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  config.traces_sample_rate = 0.002
+
+  config.send_default_pii = true
+  config.send_modules = false
+  config.rails.report_rescued_exceptions = true
+
+  config.excluded_exceptions += [
+    "ActionController::RoutingError",
+    "ActionController::UnknownFormat",
+    "ActiveRecord::RecordNotFound",
+    "Faraday::ConnectionFailed",
+    "Pundit::NotAuthorizedError",
+    "Mime::Type::InvalidMimeType"
+  ]
+end
