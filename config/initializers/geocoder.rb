5682515a3e61f9279fe753e5e11f6712c2e6aff4
Kirill Mokevnin <mokevnin@gmail.com>
2025-01-29 18:56:41 -0500

    add solid cache
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,26 +1,25 @@
-# frozen_string_literal: true
-
-# Geocoder.configure(
-#   # Geocoding options
-#   # timeout: 3,                 # geocoding service timeout (secs)
-#   # lookup: :nominatim,         # name of geocoding service (symbol)
-#
-#   # NOTE: with ipapi_com has limit 45 request/minute see https://github.com/alexreisner/geocoder/blob/master/README_API_GUIDE.md
-#   ip_lookup: :ipapi_com, # name of IP address geocoding service (symbol)
-#   # language: :en,              # ISO-639 language code
-#   # use_https: false,           # use HTTPS for lookup requests? (if supported)
-#   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
-#   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-#   # api_key: nil,               # API key for geocoding service
-#   cache: Redis.new # cache object (must respond to #[], #[]=, and #del)
-#   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
-#
-#   # Exceptions that should not be rescued by default
-#   # (if you want to implement custom error handling);
-#   # supports SocketError and Timeout::Error
-#   # always_raise: [],
-#
-#   # Calculation options
-#   # units: :mi,                 # :km for kilometers or :mi for miles
-#   # distances: :linear          # :spherical or :linear
-# )
+Geocoder.configure(
+  cache: Geocoder::CacheStore::Generic.new(Rails.cache, {})
+  #   # Geocoding options
+  #   # timeout: 3,                 # geocoding service timeout (secs)
+  #   # lookup: :nominatim,         # name of geocoding service (symbol)
+  #
+  #   # NOTE: with ipapi_com has limit 45 request/minute see https://github.com/alexreisner/geocoder/blob/master/README_API_GUIDE.md
+  #   ip_lookup: :ipapi_com, # name of IP address geocoding service (symbol)
+  #   # language: :en,              # ISO-639 language code
+  #   # use_https: false,           # use HTTPS for lookup requests? (if supported)
+  #   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
+  #   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
+  #   # api_key: nil,               # API key for geocoding service
+  #   cache: Redis.new # cache object (must respond to #[], #[]=, and #del)
+  #   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
+  #
+  #   # Exceptions that should not be rescued by default
+  #   # (if you want to implement custom error handling);
+  #   # supports SocketError and Timeout::Error
+  #   # always_raise: [],
+  #
+  #   # Calculation options
+  #   # units: :mi,                 # :km for kilometers or :mi for miles
+  #   # distances: :linear          # :spherical or :linear
+)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_28_195611) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_29_235413) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -267,6 +267,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_28_195611) do
     t.string "last_name"
     t.index ["language_id"], name: "index_reviews_on_language_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", limit: 1024, null: false
+    t.binary "value", limit: 536870912, null: false
+    t.datetime "created_at", null: false
+    t.integer "key_hash", limit: 8, null: false
+    t.integer "byte_size", limit: 4, null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
