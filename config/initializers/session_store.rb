7bb1e676c32ba768dd6ff3f1bb5ded7298e84c8f
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2025-02-17 11:25:03 +0000

    increase session lifetime

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: "_hexlet_basics_session_2"
+Rails.application.config.session_store :cookie_store, key: "_hexlet_basics_session_2",
+                                                      expire_after: 1.month
