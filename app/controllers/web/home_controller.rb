f90813c13e5927b43bcb1ecc1c720c87d9a3e287
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2025-02-25 15:32:03 +0000

    fix translation missing for home description

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -1,6 +1,5 @@
 class Web::HomeController < Web::ApplicationController
   def index
-
     language_member_resources = current_user.language_members.includes([ language: :current_version ])
       .map { |m| Language::MemberResource.new(m) }
     language_member_resources_by_language = language_member_resources.index_by { |r| r.object.language_id }
