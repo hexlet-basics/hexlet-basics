8056fadb0ba955ae28f5f6ec0a0b43f2572f673d
Andrey Groza <grozwalker@gmail.com>
2025-02-26 13:00:03 +0000

    add unsafe csp rules with report only option

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,22 +6,23 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
+    policy.connect_src :self, :https, "*", :ws, :wss
     # policy.connect_src :self, "ws://localhost:3036" if Rails.env.development?
     # Allow @vite/client to hot reload changes in development
 
-    # policy.default_src :self, :https, "giscus.app", "cdn.jsdelivr.net"
-    # policy.script_src :self, :unsafe_inline, "giscus.app", "https://cdn.jsdelivr.net"
+    policy.default_src :self, :https, "*"
+    policy.script_src :self, :https, "*", :unsafe_inline, :unsafe_eval
     # Allow @vite/client to hot reload javascript changes in development
 
     # You may need to enable this in production as well depending on your setup.
 
-    # policy.style_src :self, :unsafe_inline, "giscus.app", "https://cdn.jsdelivr.net"
+    policy.style_src :self, :https, "*", :unsafe_inline
     # Allow @vite/client to hot reload style changes in development
 
     # policy.frame_src :self, "giscus.app"
     # policy.worker_src :self, "blob:"
-    # policy.img_src     :self, :https, :data
-    #     policy.font_src    :self, :https, :data
+    policy.img_src :self, :https, "*", :data
+    policy.font_src :self, :https, "*", :data
     #     policy.object_src  :none
     #     policy.script_src  :self, :https
     # Allow @vite/client to hot reload javascript changes in development
@@ -44,5 +45,5 @@ Rails.application.configure do
   #   config.content_security_policy_nonce_directives = %w(script-src style-src)
   #
   #   # Report violations without enforcing the policy.
-  #   # config.content_security_policy_report_only = true
+  config.content_security_policy_report_only = true
 end
