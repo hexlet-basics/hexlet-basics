3cc23e410c7921e2083ef77431082f857273b44f
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2025-02-27 10:11:05 +0000

    do not load analytics plugins for ssr

--- app/javascript/analytics.ts
+++ app/javascript/analytics.ts
@@ -3,56 +3,58 @@ import postHog from "@metro-fs/analytics-plugin-posthog";
 import Analytics from "analytics";
 // import googleAnalytics from "@analytics/google-analytics"
 
+const plugins = [
+  // googleAnalytics({
+  //   measurementIds: ["G-XXXXXXXX"],
+  // }),
+  postHog({
+    token: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY,
+    enabled: true,
+    options: {
+      api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
+      debug: import.meta.env.DEV,
+      disable_session_recording: true,
+      autocapture: false,
+      capture_pageleave: true,
+      disable_surveys: true,
+      custom_campaign_params: [
+        "promo_name",
+        "promo_position",
+        "promo_type",
+        "promo_creative",
+        "promo_start",
+      ],
+    },
+  }),
+  carrotquest({
+    apiKey: import.meta.env.VITE_CARROTQUEST_API_KEY,
+    eventsMapping: {
+      signed_in: "$authorized",
+      signed_up: "$registered",
+      course_started: "Начал курс",
+      course_finished: "Завершил курс",
+      lesson_started: "Начал урок",
+      lesson_finished: "Завершил урок",
+    },
+    // Возможно, стоит сохранять все свойства
+    eventPropsMapping: {
+      id: "id",
+      email: "email",
+      locale: "locale",
+      course_slug: "course_slug",
+      lesson_slug: "lesson_slug",
+      slug: "slug",
+    },
+    userPropsMapping: {},
+  }),
+];
+
 /* Initialize analytics */
 const analytics = Analytics({
   debug: true,
   app: "hexlet-basics",
   version: 100,
-  plugins: [
-    // googleAnalytics({
-    //   measurementIds: ["G-XXXXXXXX"],
-    // }),
-    postHog({
-      token: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY,
-      enabled: !import.meta.env.SSR,
-      options: {
-        api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
-        debug: import.meta.env.DEV,
-        disable_session_recording: true,
-        autocapture: false,
-        capture_pageleave: true,
-        disable_surveys: true,
-        custom_campaign_params: [
-          "promo_name",
-          "promo_position",
-          "promo_type",
-          "promo_creative",
-          "promo_start",
-        ],
-      },
-    }),
-    carrotquest({
-      apiKey: import.meta.env.VITE_CARROTQUEST_API_KEY,
-      eventsMapping: {
-        signed_in: "$authorized",
-        signed_up: "$registered",
-        course_started: "Начал курс",
-        course_finished: "Завершил курс",
-        lesson_started: "Начал урок",
-        lesson_finished: "Завершил урок",
-      },
-      // Возможно, стоит сохранять все свойства
-      eventPropsMapping: {
-        id: "id",
-        email: "email",
-        locale: "locale",
-        course_slug: "course_slug",
-        lesson_slug: "lesson_slug",
-        slug: "slug",
-      },
-      userPropsMapping: {},
-    }),
-  ],
+  plugins: import.meta.env.SSR ? [] : plugins,
 });
 
 export default analytics;
