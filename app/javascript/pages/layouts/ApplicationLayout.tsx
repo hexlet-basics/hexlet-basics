b2797cc68748b1e8e5ece378ffcba3f9bab6c31f
Liza Chernyshova <liz4chernyshova@yandex.ru>
2025-03-11 11:06:10 +0300

    add tg contest banner

--- app/javascript/pages/layouts/ApplicationLayout.tsx
+++ app/javascript/pages/layouts/ApplicationLayout.tsx
@@ -3,10 +3,14 @@ import { Container } from "react-bootstrap";
 
 import XFlash from "@/components/XFlash.tsx";
 import { XBreadcrumb } from "@/components/breadcrumbs.tsx";
+import * as Routes from "@/routes.js";
+import type { SharedProps } from "@/types";
 import type { BreadcrumbItem } from "@/types/index.js";
+import { usePage } from "@inertiajs/react";
 import RootLayout from "./RootLayout.tsx";
 import FooterBlock from "./blocks/FooterBlock.tsx";
 import NavbarBlock from "./blocks/NavbarBlock.tsx";
+import TgContestBanner from "./tg_contest_banner/TgContestBanner.tsx";
 
 type Props = PropsWithChildren & {
   header?: string;
@@ -20,8 +24,15 @@ export default function ApplicationLayout({
   items,
   center = false,
 }: Props) {
+  const { locale } = usePage<SharedProps>().props;
+  const currentPath = window.location.pathname;
+
+  const isHomePage = currentPath === Routes.root_path();
+  const isCoursePage = currentPath.startsWith("/ru/languages/");
+
   return (
     <RootLayout>
+      {locale === "ru" && (isHomePage || isCoursePage) && <TgContestBanner />}
       <Container className="py-2">
         <NavbarBlock className="pb-3 border-bottom" />
         <XFlash />
