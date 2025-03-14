b78c6e0edb2278e3572bb2999e4e1728b2f976f2
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2025-03-14 11:22:12 +0000

    move to self hosted sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.release = "hexle-basics-platform@#{configus.hexlet_basics_release_version}"
+  config.release = configus.hexlet_basics_release_version
 
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.traces_sample_rate = 0.002
