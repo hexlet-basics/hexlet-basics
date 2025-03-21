9c639ae83e9e65716415f526a63ddae0cf71196e
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2025-03-21 16:46:35 +0000

    add openai credentials

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,8 +1,9 @@
 unless HexletBasics::Application.assets_precompiling?
   OpenAI.configure do |config|
     config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
-    config.admin_token = ENV.fetch("OPENAI_ADMIN_TOKEN") # Optional, used for admin endpoints, created here: https://platform.openai.com/settings/organization/admin-keys
-    config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID") # Optional
+    # NOTE: пока закомментировал, если будем использовать то нужно будет добавить секреты
+    # config.admin_token = ENV.fetch("OPENAI_ADMIN_TOKEN") # Optional, used for admin endpoints, created here: https://platform.openai.com/settings/organization/admin-keys
+    # config.organization_id = ENV.fetch("OPENAI_ORGANIZATION_ID") # Optional
     config.log_errors = Rails.env.development?
   end
 end
