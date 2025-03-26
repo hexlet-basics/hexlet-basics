fef486d78e1c5df64c31634a4be6fbe141f8e70d
Kirill Mokevnin <mokevnin@gmail.com>
2025-03-26 08:45:13 -0400

    fix web scope
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/repositories/language/landing_page_repository.rb
+++ app/repositories/language/landing_page_repository.rb
@@ -5,6 +5,7 @@ module Language::LandingPageRepository
     scope :with_locale, ->(locale = I18n.locale) { where(locale: locale) }
     scope :web, ->() do
       with_locale.where(listed: true)
+        .joins(:language)
         .merge(Language.completed_progress)
         .includes({ language: [ :current_version, { cover_attachment: :blob } ] })
     end
