ae1f0d3b268766c2516b24c320a79b9e08bca014
Kirill Mokevnin <mokevnin@gmail.com>
2025-03-29 08:09:30 -0400

    improve message.body output
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/javascript/pages/web/admin/messages/index.tsx
+++ app/javascript/pages/web/admin/messages/index.tsx
@@ -7,12 +7,41 @@ import AdminLayout from "@/pages/layouts/AdminLayout";
 import type { Grid, LanguageLessonMemberMessage } from "@/types/serializers";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
+import { Dialog } from "primereact/dialog";
+import { useState } from "react";
 // import { Menu } from "./shared/menu";
 
 type Props = {
   messages: LanguageLessonMemberMessage[];
   grid: Grid;
 };
+
+export function MessageBodyTemplate(message: LanguageLessonMemberMessage) {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <>
+      <a
+        type="button"
+        className="link-body-emphasis"
+        // biome-ignore lint/a11y/useValidAnchor: <explanation>
+        onClick={() => setVisible(true)}
+      >
+        {message.body?.slice(1, 50)}
+      </a>
+      <Dialog
+        visible={visible}
+        modal
+        onHide={() => {
+          if (!visible) return;
+          setVisible(false);
+        }}
+      >
+        {message.body}
+      </Dialog>
+    </>
+  );
+}
 
 export default function Index({ grid, messages }: Props) {
   const { t } = useTranslation();
@@ -38,8 +67,7 @@ export default function Index({ grid, messages }: Props) {
       >
         <Column field="id" header="id" />
         <Column field="role" header="role" />
-        <Column field="body" header="body" />
-        {/* <Column field="body" header="body" /> */}
+        <Column field="body" header="body" body={MessageBodyTemplate} />
         <Column
           field="created_at"
           header="created_at"
--- app/resources/language/lesson/member/message_resource.rb
+++ app/resources/language/lesson/member/message_resource.rb
@@ -2,12 +2,13 @@ class Language::Lesson::Member::MessageResource
   include Alba::Resource
   include Typelizer::DSL
 
-  typelize_from Language::Lesson::Member
+  typelize_from Language::Lesson::Member::Message
 
   attributes :id,
     :language_id,
     :language_lesson_id,
     :language_lesson_member_id,
-    :role
-  :body
+    :role,
+    :body,
+    :created_at
 end
