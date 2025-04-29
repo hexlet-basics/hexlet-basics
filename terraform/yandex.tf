2c3b7bf125d78d5b25aaede14b7ca3a6486da8c2
Andrey Moshkov <fakesupp@gmail.com>
2025-04-29 08:52:03 +0000

    restore storage terraform file

--- terraform/yandex.tf
+++ terraform/yandex.tf
@@ -15,21 +15,10 @@ resource "yandex_iam_service_account_static_access_key" "hb_s3_sa_static_key" {
   service_account_id = yandex_iam_service_account.hb_s3_sa.id
 }
 
-# --------------------------------------
-# STORAGE
-# --------------------------------------
-# resource "yandex_storage_bucket" "hb_sitemaps" {
-#   access_key    = yandex_iam_service_account_static_access_key.hb_s3_sa_static_key.access_key
-#   secret_key    = yandex_iam_service_account_static_access_key.hb_s3_sa_static_key.secret_key
-#   bucket        = var.yc.sitemaps_storage.bucket
-#   max_size      = 0
-#   force_destroy = false
-# }
-
-# resource "yandex_storage_bucket" "hb_storage" {
-#   access_key    = yandex_iam_service_account_static_access_key.hb_s3_sa_static_key.access_key
-#   secret_key    = yandex_iam_service_account_static_access_key.hb_s3_sa_static_key.secret_key
-#   bucket        = var.yc.storage.bucket
-#   max_size      = 0
-#   force_destroy = false
-# }
+resource "yandex_storage_bucket" "hb_storage" {
+  access_key    = yandex_iam_service_account_static_access_key.hb_s3_sa_static_key.access_key
+  secret_key    = yandex_iam_service_account_static_access_key.hb_s3_sa_static_key.secret_key
+  bucket        = var.yc.storage.bucket
+  max_size      = 0
+  force_destroy = false
+}
