ad6c382ba81b11e3af477bc20b0c0ce9fd846dce
Kirill Mokevnin <mokevnin@gmail.com>
2025-04-30 07:39:53 -0400

    guests cannot create a message
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/policies/language/lesson/member/message_policy.rb
+++ app/policies/language/lesson/member/message_policy.rb
@@ -1,5 +1,6 @@
 class Language::Lesson::Member::MessagePolicy < ApplicationPolicy
   def create?
+    return false if user.guest?
     messages_count = user.assistant_messages.where(created_at: 24.hours.ago..Time.current).count
     messages_count <= 7
   end
--- test/controllers/web/languages/lessons_controller_test.rb
+++ test/controllers/web/languages/lessons_controller_test.rb
@@ -15,11 +15,6 @@ class Web::Languages::LessonsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "show amp" do
-  #   get language_lesson_url(@language.slug, @lesson.slug, format: :amp)
-  #   assert_response :success
-  # end
-
   test "show (signed in)" do
     sign_in_as(:full)
 
@@ -40,23 +35,4 @@ class Web::Languages::LessonsControllerTest < ActionDispatch::IntegrationTest
     get language_lesson_url(@language.slug, @lesson.slug)
     assert_response :success
   end
-
-  # test "next_lesson" do
-  #   sign_in_as(:full)
-  #   third_language_lesson = language_lessons("javascript-module2-lesson1")
-  #
-  #   get next_lesson_language_lesson_url(@language.slug, @lesson.slug)
-  #   assert_response :redirect
-  #
-  #   assert_redirected_to language_lesson_url(@language.slug, third_language_lesson.slug)
-  # end
-
-  # test "prev_lesson" do
-  #   first_language_lesson = language_lessons("javascript-module1-lesson1")
-  #
-  #   get prev_lesson_language_lesson_url(@language.slug, @lesson.slug)
-  #   assert_response :redirect
-  #
-  #   assert_redirected_to language_lesson_url(@language.slug, first_language_lesson.slug)
-  # end
 end
