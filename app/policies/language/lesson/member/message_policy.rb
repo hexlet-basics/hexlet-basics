9c66921173572e17a6f98d69550bbfe4c3553a54
Andrey Moshkov <fakesupp@gmail.com>
2025-05-16 08:13:27 +0000

    change assistant from 24 hours to today count

--- app/policies/language/lesson/member/message_policy.rb
+++ app/policies/language/lesson/member/message_policy.rb
@@ -1,7 +1,7 @@
 class Language::Lesson::Member::MessagePolicy < ApplicationPolicy
   def create?
     return false if user.guest?
-    messages_count = user.assistant_messages.where(created_at: 24.hours.ago..Time.current).count
+    messages_count = user.assistant_messages.where(created_at: Time.current.beginning_of_day..).count
     messages_count <= 7
   end
 end
