d11362f7bba8decc6344373ad59afe3300835c96
Kirill Mokevnin <mokevnin@gmail.com>
2025-05-16 07:58:53 -0400

    update survey resource
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/resources/survey_resource.rb
+++ app/resources/survey_resource.rb
@@ -1,7 +1,7 @@
 class SurveyResource < ApplicationResource
   typelize_from Survey
 
-  attributes :id, :slug, :question, :description, :locale, :state
+  attributes :id, :slug, :question, :description, :locale, :state, :created_at
 
   # has_many :items, resource: Survey::ItemResource
 
