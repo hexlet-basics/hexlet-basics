db4c5f614d1e0f0829635f2bd909867718404fcb
Andrey Moshkov <fakesupp@gmail.com>
2025-05-19 12:29:01 +0000

    fix message policy

--- app/policies/language/lesson/member/message_policy.rb
+++ app/policies/language/lesson/member/message_policy.rb
@@ -1,7 +1,9 @@
 class Language::Lesson::Member::MessagePolicy < ApplicationPolicy
   def create?
     return false if user.guest?
-    messages_count = user.assistant_messages.where(created_at: Time.current.beginning_of_day..).count
+    messages_count = user.assistant_messages
+      .user_role
+      .where(created_at: Time.current.beginning_of_day..).count
     messages_count <= 7
   end
 end
