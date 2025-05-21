9c7f7d9a17c40bf74ec8ac47e279e0fdfaed549b
Kirill Mokevnin <mokevnin@gmail.com>
2025-05-21 06:56:29 -0400

    improve layout
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/javascript/pages/layouts/blocks/ContactMethodRequestingBlock.tsx
+++ app/javascript/pages/layouts/blocks/ContactMethodRequestingBlock.tsx
@@ -1,14 +1,20 @@
 import XssContent from "@/components/XssContent";
 import * as Routes from "@/routes.js";
-import { Alert } from "react-bootstrap"
+import { Link } from "@inertiajs/react";
+import { Alert, Button } from "react-bootstrap"
 import { useTranslation } from "react-i18next"
 
 export default function ContactMethodRequestingBlock() {
   const { t: tLayouts } = useTranslation("layouts")
 
   return <Alert variant="light">
-    <XssContent>
-      {tLayouts('shared.contact_method_requesting.description_html', { url: Routes.edit_account_profile_url() })}
-    </XssContent>
+    <div className="d-flex">
+      <XssContent className="my-auto me-auto">
+        {tLayouts('shared.contact_method_requesting.description_html', { url: Routes.edit_account_profile_url() })}
+      </XssContent>
+      <Link href={Routes.edit_account_profile_url()} className="btn-outline-primary text-decoration-none">
+        {tLayouts('shared.contact_method_requesting.go')}
+      </Link>
+    </div>
   </Alert>
 }
