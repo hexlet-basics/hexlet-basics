268c55f21111debda9b96d4b41ea55e2f607f103
Andrey Moshkov <fakesupp@gmail.com>
2025-05-21 07:49:57 +0000

    fix test after update fixtures

--- test/controllers/web/account/profiles_controller_test.rb
+++ test/controllers/web/account/profiles_controller_test.rb
@@ -27,7 +27,7 @@ class Web::Account::ProfilesControllerTest < ActionDispatch::IntegrationTest
 
     assert { user.first_name == new_name }
     assert { user.lead }
-    assert { user.lead.courses_data.include?({ slug: "elixir", lessons_finished_count: 2 }) }
+    assert { user.lead.courses_data.include?({ slug: "elixir", lessons_finished_count: 3 }) }
     assert { user.lead.survey_answers_data.include?({ question: "Какую задачу вы решаете?", answer: "Планирую поменять карьеру" }) }
   end
 
