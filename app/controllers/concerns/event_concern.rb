53bbd5189f89816ac233ce1f7aef285f34f29fad
Kirill Mokevnin <mokevnin@gmail.com>
2025-05-29 07:03:34 -0400

    use db for happend events
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/controllers/concerns/event_concern.rb
+++ app/controllers/concerns/event_concern.rb
@@ -6,47 +6,56 @@ module EventConcern
       after_action :clean_js_events
 
       inertia_share do
-        happendEvents = session[:happendEvents]
-        { happendEvents: }
+        if session[:happened_event_ids]
+          events = load_events_from_store(session[:happened_event_ids])
+          {
+            happenedEvents: events.map do |event|
+              {
+                id: event.event_id,
+                type: event.event_type,
+                data: event.data
+              }
+            end
+          }
+        else
+          { happenedEvents: [] }
+        end
       end
     end
   end
 
   def event_to_js(event)
-    session[:happendEvents] ||= []
-    eventData = {
-      id: event.event_id,
-      type: event.event_type,
-      data: event.data
-    }
-    session[:happendEvents] << eventData
-    # EventsMapping.exists!(event_name)
-    # session[:fired_events][event_name] = options
+    session[:happened_event_ids] ||= []
+    session[:happened_event_ids] << event.event_id
   end
 
   def clean_js_events
-    # NOTE: могут быть двойные редиректы и стираем данные только
-    # при отрисовке хтмл страницы, а не запросу по апи
-    if response.successful? # && response.content_type.include?("text/html")
-      session[:happendEvents] = nil
+    # Стираем только при HTML-ответе (не API)
+    if response.successful?
+      session[:happened_event_ids] = nil
     end
   end
 
   def publish_event(event, user)
     if user.guest?
       event_store.publish(event)
-      return
+    else
+      event_store.with_metadata(user_id: user.id) do
+        event_store.publish(
+          event,
+          stream_name: "user-#{user.id}",
+        )
+      end
     end
 
-    event_store.with_metadata(user_id: user.id) do
-      event_store.publish(
-        event,
-        stream_name: "user-#{user.id}",
-      )
-    end
+    event_to_js(event)
   end
 
   def event_store
     Rails.configuration.event_store
+  end
+
+  def load_events_from_store(event_ids)
+    event_ids.map { |id| event_store.read.event(id) }
   end
 end
