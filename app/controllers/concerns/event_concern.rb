2bc9cf95b845ad1d2bcad8ee90a810524328a0f1
Andrey Groza <grozwalker@gmail.com>
2025-05-29 11:33:28 +0000

    fix happend events

--- app/controllers/concerns/event_concern.rb
+++ app/controllers/concerns/event_concern.rb
@@ -6,20 +6,18 @@ module EventConcern
       after_action :clean_js_events
 
       inertia_share do
-        if session[:happened_event_ids]
-          events = load_events_from_store(session[:happened_event_ids])
-          {
-            happenedEvents: events.map do |event|
-              {
-                id: event.event_id,
-                type: event.event_type,
-                data: event.data
-              }
-            end
-          }
-        else
-          { happenedEvents: [] }
-        end
+        happened_event_ids = session[:happened_event_ids] || []
+        events = event_store.read.events(happened_event_ids)
+
+        {
+          happendEvents: events.map do |event|
+            {
+              id: event.event_id,
+              type: event.event_type,
+              data: event.data
+            }
+          end
+        }
       end
     end
   end
@@ -53,9 +51,5 @@ module EventConcern
 
   def event_store
     Rails.configuration.event_store
-  end
-
-  def load_events_from_store(event_ids)
-    event_ids.map { |id| event_store.read.event(id) }
   end
 end
