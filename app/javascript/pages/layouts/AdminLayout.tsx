7aeb15bdd25529aaeeb5f601a1b2bed796adfa58
Kirill Mokevnin <mokevnin@gmail.com>
2025-06-18 15:19:46 -0400

    fix admin menu
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/javascript/pages/layouts/AdminLayout.tsx
+++ app/javascript/pages/layouts/AdminLayout.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
-import { AppShell, Container, Grid, NavLink, Title } from '@mantine/core';
+import { AppShell, Box, Button, Container, Drawer, Grid, Group, NavLink, Title } from '@mantine/core';
 
 import { useTranslation } from "react-i18next";
 
@@ -16,11 +16,100 @@ type Props = PropsWithChildren & {
   header: string;
 };
 
+function NavbarMenu() {
+  const { t: tLayouts } = useTranslation("layouts");
+
+  return (<>
+    <NavLink
+      component={Link}
+      href={Routes.admin_root_path()}
+      label={tLayouts("web.admin.application.dashboard")}
+      leftSection={<i className="bi bi-house" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_language_categories_path()}
+      label={tLayouts("web.admin.application.language_categories")}
+      leftSection={<i className="bi bi-file-text" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_languages_path()}
+      label={tLayouts("web.admin.application.languages")}
+      leftSection={<i className="bi bi-terminal" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_language_landing_pages_path()}
+      label={tLayouts("web.admin.application.language_landing_pages")}
+      leftSection={<i className="bi bi-terminal" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_reviews_path()}
+      label={tLayouts("web.admin.application.reviews")}
+      leftSection={<i className="bi bi-chat-left-quote" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_blog_posts_path()}
+      label={tLayouts("web.admin.application.blog_posts")}
+      leftSection={<i className="bi bi-file-text" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_messages_path()}
+      label={tLayouts("web.admin.application.messages")}
+      leftSection={<i className="bi bi-file-text" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_language_lesson_members_path()}
+      label={tLayouts("web.admin.application.language_lesson_members")}
+      leftSection={<i className="bi bi-file-text" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_surveys_path()}
+      label={tLayouts("web.admin.application.surveys")}
+      leftSection={<i className="bi bi-patch-question" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_survey_scenarios_path()}
+      label={tLayouts("web.admin.application.survey_scenarios")}
+      leftSection={<i className="bi bi-collection" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_survey_answers_path()}
+      label={tLayouts("web.admin.application.survey_answers")}
+      leftSection={<i className="bi bi-signpost-2" />}
+    />
+    <NavLink
+      component={Link}
+      href={Routes.admin_leads_url()}
+      label={tLayouts("web.admin.application.leads")}
+      leftSection={<i className="bi bi-person-check" />}
+    />
+
+    <hr />
+
+    <NavLink
+      component={Link}
+      href={Routes.admin_management_users_path()}
+      label={tLayouts("web.admin.application.users")}
+      leftSection={<i className="bi bi-people" />}
+    />
+  </>)
+}
+
 export default function AdminLayout({ children, header }: Props) {
   const { t: tLayouts } = useTranslation("layouts");
-  const { t: tCommon } = useTranslation("common");
+  const [mobileOpened,] = useDisclosure();
 
-  const [opened, { toggle }] = useDisclosure();
+  const [navbarOpened, { toggle: toggleNavbar }] = useDisclosure();
+  const [opened, { open, close }] = useDisclosure();
 
   return (
     <RootLayout>
@@ -29,95 +118,24 @@ export default function AdminLayout({ children, header }: Props) {
         navbar={{
           width: 250,
           breakpoint: 'xs',
+          collapsed: { mobile: !mobileOpened }
         }}
         padding="lg"
       >
         <AppShell.Header>
-          <NavbarBlock onToggle={toggle} opened={opened} />
+          <NavbarBlock onToggle={toggleNavbar} opened={navbarOpened} />
+          <Drawer opened={opened} onClose={close} title="Authentication">
+            <NavbarMenu />
+          </Drawer>
+          <Group justify="end">
+            <Button m="xs" variant="default" onClick={open} hiddenFrom="sm">
+              Menu
+            </Button>
+          </Group>
         </AppShell.Header>
 
         <AppShell.Navbar>
-          <NavLink
-            component={Link}
-            href={Routes.admin_root_path()}
-            label={tLayouts("web.admin.application.dashboard")}
-            leftSection={<i className="bi bi-house" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_language_categories_path()}
-            label={tLayouts("web.admin.application.language_categories")}
-            leftSection={<i className="bi bi-file-text" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_languages_path()}
-            label={tLayouts("web.admin.application.languages")}
-            leftSection={<i className="bi bi-terminal" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_language_landing_pages_path()}
-            label={tLayouts("web.admin.application.language_landing_pages")}
-            leftSection={<i className="bi bi-terminal" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_reviews_path()}
-            label={tLayouts("web.admin.application.reviews")}
-            leftSection={<i className="bi bi-chat-left-quote" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_blog_posts_path()}
-            label={tLayouts("web.admin.application.blog_posts")}
-            leftSection={<i className="bi bi-file-text" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_messages_path()}
-            label={tLayouts("web.admin.application.messages")}
-            leftSection={<i className="bi bi-file-text" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_language_lesson_members_path()}
-            label={tLayouts("web.admin.application.language_lesson_members")}
-            leftSection={<i className="bi bi-file-text" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_surveys_path()}
-            label={tLayouts("web.admin.application.surveys")}
-            leftSection={<i className="bi bi-patch-question" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_survey_scenarios_path()}
-            label={tLayouts("web.admin.application.survey_scenarios")}
-            leftSection={<i className="bi bi-collection" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_survey_answers_path()}
-            label={tLayouts("web.admin.application.survey_answers")}
-            leftSection={<i className="bi bi-signpost-2" />}
-          />
-          <NavLink
-            component={Link}
-            href={Routes.admin_leads_url()}
-            label={tLayouts("web.admin.application.leads")}
-            leftSection={<i className="bi bi-person-check" />}
-          />
-
-          <hr />
-
-          <NavLink
-            component={Link}
-            href={Routes.admin_management_users_path()}
-            label={tLayouts("web.admin.application.users")}
-            leftSection={<i className="bi bi-people" />}
-          />
+          <NavbarMenu />
         </AppShell.Navbar>
 
         <AppShell.Main>
@@ -133,3 +151,5 @@ export default function AdminLayout({ children, header }: Props) {
     </RootLayout>
   );
 }
+
+
