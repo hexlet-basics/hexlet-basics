d8ffe8e50b123d0a3f9b39fe00ef50ddcd0d803b
Kirill Mokevnin <mokevnin@gmail.com>
2025-06-27 13:44:47 -0500

    handle pagy exceptions
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class ApplicationController < ActionController::Base
   include ActiveStorage::SetCurrent
   include Pundit::Authorization
   include Pagy::Backend
+  rescue_from Pagy::OverflowError, with: :redirect_to_last_page
+  rescue_from Pagy::VariableError, with: :redirect_to_last_page
 
   include AuthConcern
 
   def default_url_options
     { suffix: params[:suffix] }
+  end
+
+  def redirect_to_last_page(exception)
+    redirect_to url_for(page: exception.pagy.last), notice: "Page ##{params[:page]} is overflowing. Showing page #{exception.pagy.last} instead."
   end
 end
