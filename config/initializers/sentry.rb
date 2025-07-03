2ad0b0e43901b5584ff05573494f351c6b3dd6fc
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2025-07-03 07:52:24 +0000

    set recommended rate for sentry becauseof high amount of requests

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,8 +2,8 @@ Sentry.init do |config|
   config.release = configus.hexlet_basics_release_version
 
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.traces_sample_rate = 0.1
-  config.profiles_sample_rate = 0.1
+  config.traces_sample_rate = 0.01
+  config.profiles_sample_rate = 0.01
 
   config.send_default_pii = true
   config.send_modules = false
