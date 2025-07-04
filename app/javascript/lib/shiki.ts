c101f68aed942e055bf00c4fec8b70b5ae7dd6d9
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2025-07-04 15:34:36 +0000

    create highlighter instance once

--- app/javascript/lib/shiki.ts
+++ app/javascript/lib/shiki.ts
@@ -1,47 +1,48 @@
 import { createHighlighterCore } from 'shiki/core';
 import { createJavaScriptRegexEngine } from 'shiki/engine/javascript';
 
-const getHighlighter = async () => {
-  const highlighter = await createHighlighterCore({
-    langs: [
-      import('@shikijs/langs/tsx'),
-      import('@shikijs/langs/swift'),
-      import('@shikijs/langs/rust'),
-      import('@shikijs/langs/scss'),
-      import('@shikijs/langs/css'),
-      import('@shikijs/langs/html'),
-      import('@shikijs/langs/bash'),
-      import('@shikijs/langs/json'),
-      import('@shikijs/langs/elixir'),
-      import('@shikijs/langs/clojure'),
-      import('@shikijs/langs/racket'),
-      import('@shikijs/langs/1c'),
-      // import('@shikijs/langs/c'),
-      // import('@shikijs/langs/cpp'),
-      import('@shikijs/langs/haskell'),
-      import('@shikijs/langs/js'),
-      import('@shikijs/langs/ts'),
-      import('@shikijs/langs/jsx'),
-      import('@shikijs/langs/php'),
-      import('@shikijs/langs/java'),
-      import('@shikijs/langs/ruby'),
-      import('@shikijs/langs/csharp'),
-      import('@shikijs/langs/go'),
-      import('@shikijs/langs/python'),
-      import('@shikijs/langs/scheme'),
-      import('@shikijs/langs/perl'),
-    ],
-    themes: [
-      import('@shikijs/themes/github-dark'),
-      import('@shikijs/themes/github-light'),
-    ],
-    engine: createJavaScriptRegexEngine(),
-  })
-
+const highlighterPromise = createHighlighterCore({
+  langs: [
+    import('@shikijs/langs/tsx'),
+    import('@shikijs/langs/swift'),
+    import('@shikijs/langs/rust'),
+    import('@shikijs/langs/scss'),
+    import('@shikijs/langs/css'),
+    import('@shikijs/langs/html'),
+    import('@shikijs/langs/bash'),
+    import('@shikijs/langs/json'),
+    import('@shikijs/langs/elixir'),
+    import('@shikijs/langs/clojure'),
+    import('@shikijs/langs/racket'),
+    import('@shikijs/langs/1c'),
+    // import('@shikijs/langs/c'),
+    // import('@shikijs/langs/cpp'),
+    import('@shikijs/langs/haskell'),
+    import('@shikijs/langs/js'),
+    import('@shikijs/langs/ts'),
+    import('@shikijs/langs/jsx'),
+    import('@shikijs/langs/php'),
+    import('@shikijs/langs/java'),
+    import('@shikijs/langs/ruby'),
+    import('@shikijs/langs/csharp'),
+    import('@shikijs/langs/go'),
+    import('@shikijs/langs/python'),
+    import('@shikijs/langs/scheme'),
+    import('@shikijs/langs/perl'),
+  ],
+  themes: [
+    import('@shikijs/themes/github-dark'),
+    import('@shikijs/themes/github-light'),
+  ],
+  engine: createJavaScriptRegexEngine(),
+}).then((highlighter) => {
   highlighter.getTheme('github-light').bg = 'var(--mantine-color-gray-0)';
   highlighter.getTheme('github-dark').bg = 'var(--mantine-color-gray-7)';
 
   return highlighter;
-};
+});
 
-export default getHighlighter;
+export default async () => {
+  const highlighter = await highlighterPromise
+  return highlighter;
+}
