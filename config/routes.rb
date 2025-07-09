387fcce0fe58ba03300310c2e96873bbbe359d0e
Kirill Mokevnin <mokevnin@gmail.com>
2025-07-09 17:55:58 -0500

    add redirect routes
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  get "*path.amp", to: redirect("/%{path}", status: 301)
+  # get "*path.amp", to: redirect("/%{path}", status: 301)
+  get "*path.amp", to: redirect { |params, _|
+    "/#{params[:path]}"
+  }
 
   # Redirects (Old routes/SEO)
   get "/:locale/languages/:lang/modules/:module/lessons/:lesson", to: redirect("/%{locale}/languages/%{lang}/lessons/%{lesson}")
 
   get "/ru/languages/pre-course-python", to: redirect("/ru/languages/python")
   get "/ru/languages/pre-course-python/*", to: redirect("/ru/languages/python")
+
+  get "/ru/languages/layout-designer", to: redirect("/ru/languages/html")
+  get "/ru/languages/layout-designer/*", to: redirect("/ru/languages/html")
 
   get "/ru/languages/pre-course-java", to: redirect("/ru/languages/java")
   get "/ru/languages/pre-course-java/*", to: redirect("/ru/languages/java")
