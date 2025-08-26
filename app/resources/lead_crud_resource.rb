c96ec5579a43b5335300a055da492eeb94632672
Kirill Mokevnin <mokevnin@gmail.com>
2025-08-26 09:36:27 -0400

    some updates
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/resources/lead_crud_resource.rb
+++ app/resources/lead_crud_resource.rb
@@ -5,8 +5,8 @@ class LeadCrudResource < ApplicationResource
   attributes :contact_method, :contact_value, :ym_client_id
 
   # TODO: use enum
-  typelize contact_method: '"telegram" | "telephone" | "whatsapp"',
-    contact_value: :string
+  # typelize contact_method: '"telegram" | "telephone" | "whatsapp"',
+  #   contact_value: :string
 
   typelize_meta meta: "{ modelName: string }"
   meta do
