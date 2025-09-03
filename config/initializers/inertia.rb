b00954f553685268a64a2996f948a47d9c5d9619
Kirill Mokevnin <mokevnin@gmail.com>
2025-09-03 13:41:56 -0400

    update inertia config
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- config/initializers/inertia.rb
+++ config/initializers/inertia.rb
@@ -1,5 +1,6 @@
 InertiaRails.configure do |config|
   # Example: force a full-reload if the deployed assets change.
   config.ssr_enabled = ViteRuby.config.ssr_build_enabled
+  config.always_include_errors_hash = true
   config.version = ViteRuby.digest
 end
