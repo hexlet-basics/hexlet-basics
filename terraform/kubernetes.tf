5c252b464f7c420b5b2a0471b01a4ddedf050a95
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2025-09-12 22:45:30 +0300

    improve infrasture config

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -9,6 +9,7 @@ resource "yandex_kubernetes_node_group" "code_basics_node_group_2" {
 
   node_labels = {
     "group" = "codebasics"
+    "role" = "web"
   }
 
   instance_template {
@@ -38,7 +39,59 @@ resource "yandex_kubernetes_node_group" "code_basics_node_group_2" {
 
   scale_policy {
     fixed_scale {
-      size = 3
+      size = 2
+    }
+  }
+
+  deploy_policy {
+    max_expansion   = 1
+    max_unavailable = 0
+  }
+
+  maintenance_policy {
+    auto_upgrade = true
+    auto_repair  = true
+  }
+}
+
+resource "yandex_kubernetes_node_group" "code_basics_node_group_3" {
+  cluster_id = data.yandex_kubernetes_cluster.kube_cluster_1.id
+  name       = "code-basics-node-group-3"
+  version    = data.yandex_kubernetes_cluster.kube_cluster_1.master[0].version
+
+  node_labels = {
+    "group" = "codebasics"
+    "role" = "service"
+  }
+
+  instance_template {
+    platform_id = "standard-v3"
+
+    network_interface {
+      nat = false
+      subnet_ids = [
+        yandex_vpc_subnet.code_basics_b_1.id,
+      ]
+    }
+
+    resources {
+      memory = 4
+      cores  = 2
+    }
+
+    boot_disk {
+      type = "network-ssd-nonreplicated"
+      size = 93
+    }
+
+    container_runtime {
+      type = "containerd"
+    }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 1
     }
   }
 
