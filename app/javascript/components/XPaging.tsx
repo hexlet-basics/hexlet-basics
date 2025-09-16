854c48a6fbc7828b0368d78f53b10dff00d4f898
Andrey Usyaev <andrey.usyaev@gmail.com>
2025-09-16 13:56:04 +0300

    Fix code review remarks

--- app/javascript/components/XPaging.tsx
+++ app/javascript/components/XPaging.tsx
@@ -1,4 +1,4 @@
-import { Link, router, usePage } from '@inertiajs/react';
+import { Link, usePage } from '@inertiajs/react';
 import { Center, Pagination } from '@mantine/core';
 import type { PropsWithChildren } from 'react';
 import { fromWindow } from '@/lib/utils.ts';
@@ -13,46 +13,31 @@ const defaultInertiaLinkOptions = {
   preserveState: true,
 };
 
+const paginationUrl = (page: number, url: string) => {
+  const origin = fromWindow('location')?.origin;
+  if (!origin) {
+    return;
+  }
+
+  const u = new URL(url, origin);
+  u.searchParams.set('page', String(page));
+  return u.toString();
+};
+
+const linkComponent = (page: number, url: string) => {
+  const linkHref = paginationUrl(page, url);
+
+  return linkHref
+    ? {
+        component: Link,
+        href: linkHref,
+        ...defaultInertiaLinkOptions,
+      }
+    : {};
+};
+
 export default function XPaging({ pagy }: Props) {
   const { url } = usePage<SharedProps>();
-  const origin = fromWindow('location')?.origin;
-
-  const paginationUrl = (page: number) => {
-    const u = new URL(url, origin);
-    u.searchParams.set('page', String(page));
-    return u.toString();
-  };
-
-  const linkComponent = (page: number) => {
-    return {
-      component: Link,
-      href: paginationUrl(page),
-      ...defaultInertiaLinkOptions,
-    };
-  };
-
-  const controlUrl = (control: string) => {
-    switch (control) {
-      case 'next':
-        return pagy.page < pagy.last
-          ? {
-              component: Link,
-              href: paginationUrl(pagy.page + 1),
-              ...defaultInertiaLinkOptions,
-            }
-          : {};
-      case 'previous':
-        return pagy.page > 1
-          ? {
-              component: Link,
-              href: paginationUrl(pagy.page - 1),
-              ...defaultInertiaLinkOptions,
-            }
-          : {};
-      default:
-        return {};
-    }
-  };
 
   return (
     <Center>
@@ -60,8 +45,11 @@ export default function XPaging({ pagy }: Props) {
         my="xl"
         total={pagy.last}
         value={pagy.page}
-        getItemProps={(page) => linkComponent(page)}
-        getControlProps={(control) => controlUrl(control)}
+        getItemProps={(page) => linkComponent(page, url)}
+        getControlProps={(control) => {
+          const page = pagy[control];
+          return page ? linkComponent(page, url) : {};
+        }}
         boundaries={1}
         siblings={1}
       />
