689a53d92958685c5a94aa1d22b7607241c698cd
Ivan Korolev <zerkc_97@mail.ru>
2025-10-10 13:02:22 +0000

    Keep user on the same page when switching locales

--- app/controllers/web/locales_controller.rb
+++ app/controllers/web/locales_controller.rb
@@ -5,7 +5,6 @@ class Web::LocalesController < Web::ApplicationController
 
   def switch
     locale = params[:new_locale]
-    # redirect_path = requst.referer || root_path
 
     unless I18n.available_locales.include?(locale&.to_sym)
       redirect_back fallback_location: root_path(suffix: AppHost.locale_for_url(I18n.default_locale))
@@ -19,6 +18,7 @@ class Web::LocalesController < Web::ApplicationController
 
     session[:locale] = locale
 
-    redirect_to root_url(suffix: AppHost.locale_for_url(locale)), allow_other_host: true
+    route_params = Rails.application.routes.recognize_path(request.referer)
+    redirect_to url_for(**route_params, suffix: AppHost.locale_for_url(locale))
   end
 end
